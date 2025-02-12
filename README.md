7a14b9086444701f659ed1a5c89301568670742e
Milan <milantony2005@gmail.com>
2025-02-12 17:40:47 +0530

    final

---
+++ README.md
@@ -0,0 +1,1 @@
+React component called 'ContentRating' where the component will let users rate material by clicking 'like' or 'dislike' buttons.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ContentRating from "./Components/ContentRating"
+ function App() {
+   return (
+    <>
+   <ContentRating/>
+    </>
+   )
+ }
+ export default App
